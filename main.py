35d35429b68976f6ada7f4d58c66b9bed8c184c9
Renana Turgeman <renanaturgeman058@gmail.com>
2024-07-12 15:45:46 +0300

    add helper functions

--- main.py
+++ main.py
@@ -26,3 +26,35 @@ with open("cifar-10-python.tar.gz", "wb") as file:
 
 with tarfile.open("cifar-10-python.tar.gz", "r:gz") as tar:
     tar.extractall()
+
+
+def load_cifar_batch(file_path):
+    """
+    Loads a CIFAR-10 batch file and returns the data as a dictionary.
+
+    :param: file_path : The path to the CIFAR-10 batch file.
+
+    :return: dict:  A dictionary containing the data and labels from the CIFAR-10 batch file.
+                    The dictionary contains the following keys:
+                        - b'data': A numpy array of shape (10000, 3072) containing the image data.
+                        - b'labels': A list of length 10000 containing the labels for the images.
+                        - Other metadata keys such as b'batch_label' and b'filenames'.
+    """
+    with open(file_path, 'rb') as file:
+        data_dict = np.load(file, encoding='bytes', allow_pickle=True)
+    return data_dict
+
+
+def normalize_data(train, val, test):
+    """
+    Normalizes the training, validation, and test datasets by scaling the pixel values to be between 0 and 1.
+
+    :param: train :  The training dataset.
+    :param: val : The validation dataset.
+    :param: test : The test dataset.
+
+    This function modifies the input datasets in place by dividing each element by 255.0.
+    """
+    train = train / 255.0
+    val = val / 255.0
+    test = test / 255.0
