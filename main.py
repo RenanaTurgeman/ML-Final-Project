8999cba0d2587dca634979329fa6db58bf8402ce
Renana Turgeman <renanaturgeman058@gmail.com>
2024-07-12 10:13:21 +0300

    continue understanding the data

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ import tarfile
 import pickle
 import os
 
+#################### LOAD THE DATA ###########################
+
 # Download the CIFAR-10 dataset
 url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
 response = requests.get(url, stream=True)
@@ -27,11 +29,13 @@ with open("cifar-10-python.tar.gz", "wb") as file:
 with tarfile.open("cifar-10-python.tar.gz", "r:gz") as tar:
     tar.extractall()
 
+
 # Load the data from the extracted files
 def unpickle(file):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
     return dict
+
 
 train_data = []
 train_labels = []
@@ -54,7 +58,8 @@ label_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', '
 # Map numerical labels to class names
 train_labels_df = train_labels_df.map(lambda x: label_names[x])
 
-# Function to plot the distribution of labels in the training data
+
+################### UNDERSTANDING THE DATA ##################################
 def distribution_of_labels():
     """
     Function to plot the distribution of labels in the training data
@@ -67,6 +72,7 @@ def distribution_of_labels():
     plt.xticks(rotation=45)
     plt.show()
 
+
 def example_of_images():
     # Display examples of images in the data
     fig, axes = plt.subplots(2, 5, figsize=(10, 5))
@@ -78,8 +84,74 @@ def example_of_images():
 
     plt.show()
 
+
+def basic_statistics():
+    """
+    Function to display basic statistics of the dataset
+    """
+    print(train_data_df.describe())
+
+
+def random_sample_images():
+    """
+    Function to display a random sample of images from the dataset
+    """
+    sample_indices = np.random.choice(train_data_df.index, size=10, replace=False)
+    fig, axes = plt.subplots(2, 5, figsize=(10, 5))
+    for i, ax in enumerate(axes.flat):
+        img = train_data_df.iloc[sample_indices[i]].values.reshape(3, 32, 32).transpose(1, 2, 0)
+        ax.imshow(img)
+        ax.set_title(f"Label: {train_labels_df.iloc[sample_indices[i]]}")
+        ax.axis('off')
+    plt.show()
+
+
+def pixel_value_distribution():
+    """
+    Function to plot the distribution of pixel values in the dataset
+    """
+    pixel_values = train_data_df.values.flatten()
+    plt.hist(pixel_values, bins=50, color='blue', alpha=0.7)
+    plt.title('Distribution of Pixel Values')
+    plt.xlabel('Pixel Value')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+def normalize_images():
+    """
+    Function to normalize the images and display a random sample of normalized images
+    """
+    normalized_data = train_data_df / 255.0
+    sample_indices = np.random.choice(normalized_data.index, size=10, replace=False)
+    fig, axes = plt.subplots(2, 5, figsize=(10, 5))
+    for i, ax in enumerate(axes.flat):
+        img = normalized_data.iloc[sample_indices[i]].values.reshape(3, 32, 32).transpose(1, 2, 0)
+        ax.imshow(img)
+        ax.set_title(f"Label: {train_labels_df.iloc[sample_indices[i]]}")
+        ax.axis('off')
+    plt.show()
+
+
+def pixel_statistics_per_channel():
+    """
+    Function to display the mean and standard deviation of pixel values per channel (RGB)
+    """
+    means = train_data_df.mean(axis=0).values.reshape(3, 32, 32).mean(axis=(1, 2))
+    stds = train_data_df.std(axis=0).values.reshape(3, 32, 32).std(axis=(1, 2))
+    print("Mean pixel values per channel (R, G, B):", means)
+    print("Standard deviation of pixel values per channel (R, G, B):", stds)
+
+
 def understanding_the_data():
     distribution_of_labels()
     example_of_images()
+    basic_statistics()
+    random_sample_images()
+    pixel_value_distribution()
+    normalize_images()
+    pixel_statistics_per_channel()
 
+
+# Call the understanding_the_data function to perform all analyses
 understanding_the_data()
