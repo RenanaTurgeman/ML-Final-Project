c85601abc4a792daa09221ad9d5807eb2807395a
Renana Turgeman <renanaturgeman058@gmail.com>
2024-07-19 12:44:15 +0300

    histograma.py

---
+++ histograma.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+categories = ['KNN', 'Logistic Regression', 'SVM', 'Decision Tree']
+accuracy_color = [0.32,  0.3465,  0.4274,  0.2590]
+accuracy_gray = [0.28, 0.2645, 0.3522, 0.2152]
+
+fig, ax = plt.subplots()
+width = 0.35
+x = range(len(categories))
+
+bars1 = ax.bar(x, accuracy_color, width, label='Color Images')
+bars2 = ax.bar([p + width for p in x], accuracy_gray, width, label='Grayscale Images')
+
+ax.set_xlabel('Categories')
+ax.set_ylabel('Accuracy')
+ax.set_title('Accuracy by Image Type')
+ax.set_xticks([p + width/2 for p in x])
+ax.set_xticklabels(categories)
+ax.legend()
+
+plt.show()
